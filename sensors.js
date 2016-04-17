9a9ec9a029aff1fc807d1872221398aa39c9b86e
Tomi Hakkarainen <tpaivaa@gmail.com>
2016-04-17 22:13:32 +0300

    write Soittila temps to Firebase

---
+++ sensors.js
@@ -0,0 +1,16 @@
+module.exports = [
+					['ulkoE','/28.C6FA8E040000', 'Ulkolämpötila'],
+					['varaajaMiddle','/28.FFED0E340400','Lämmitysvesi Varaaja'],
+					['greeWaterCold','/28.FF7C4E330400','Lämpöpumppu Vesi'],
+					['greeGasCold','/28.FF014E310400','Lämpöpumppu Kaasu'],
+					['khh','/10.C0AB5B020800','Kodinhoitohuone'],
+					['khhLattia','/28.FF6250330400','Kodinhoitohuone Lattia'],
+					['keittio','/28.52B48E040000','Keittiö'],
+					['keittioLattia','/28.FF4952330400','Keittiö Lattia'],
+					['olohuone','/10.A6815B020800','Olohuone'],
+					['makuuhuone','/10.52B25B020800','Makuuhuone'],
+					['ykMH','/28.FFC94E330400','Yläkerta Makuuhuone'],
+					['ykAula','/28.FF570F340400','Yläkerta Aula'],
+					['verantaLattia','/28.FF8A31430400','Verannan Lattia'],
+					['veranta','/28.FF2859330400','Veranta']
+]
