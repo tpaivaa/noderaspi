8f59f9bdb8b88dea60c254328f83da35af669c19
Tomi Hakkarainen <tpaivaa@gmail.com>
2016-04-17 19:28:01 +0300

    getting started with firebase

---
+++ util.js
@@ -0,0 +1,13 @@
+var Firebase = require('firebase');
+var Ref = new Firebase('https://soittila.firebase.com/');
+
+
+function updatetemps(sensors) {
+	for ( i in sensors){
+		Ref.
+	}
+};
+
+module.exports = {
+	updatetemps: updatetemps
+}
